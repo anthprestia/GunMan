80b6820659dba149124f8812d49e5c77d405ee70
anthprestia <anthprestia@gmail.com>
2021-03-26 13:36:37 -0400

    Jumping Mechanic has been added.
    Using a Box collider you check to see if the player is grounded. If so then they can jump otherwise they can not. The player can change directions in mid air without losing velocity.

--- GunMan/Assets/Scripts/PlayerController.cs
+++ GunMan/Assets/Scripts/PlayerController.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    [SerializeField] private LayerMask platformLayerMask;
+    private Rigidbody2D rigidbody2d;
+    private BoxCollider2D boxCollider2d;
     public GameObject bulletPrefab;
     private Animator anim;
+
+    public float speed = 5.0f;
+    public float jumpHeight = 10.0f;
+
     public bool isWalking;
-    public float moveInput;
+    public float runInput;
+    public float verticalInput;
+    
 
     public Transform firepoint;
 
@@ -17,22 +25,43 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         anim = GetComponent<Animator>();
+        rigidbody2d = transform.GetComponent<Rigidbody2D>();
+        boxCollider2d = transform.GetComponent<BoxCollider2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveInput = Input.GetAxisRaw("Horizontal");
-        playAnimation(moveInput);
+        runInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        playAnimation(runInput, verticalInput);
 
         // Move the character left or right
-        transform.Translate(Vector3.right * Time.deltaTime * speed *  Math.Abs(moveInput));
 
+        transform.Translate(Vector3.right * Time.deltaTime * speed * Math.Abs(runInput));
+        //rigidbody2d.velocity = new Vector3(speed * runInput, rigidbody2d.velocity.y);
+        
+        // Make the character jump or crouch
+        if (verticalInput > 0 && isGrounded())
+        {
+            rigidbody2d.velocity = Vector2.up * jumpHeight;
+        }
     }
 
-    void playAnimation(float moveInput)
+    private bool isGrounded()
     {
-        if (moveInput == 0)
+
+        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, 0.1f, platformLayerMask);
+
+        Debug.Log(raycastHit2d.collider);
+        return raycastHit2d.collider != null;
+    }
+
+    void playAnimation(float runInput, float verticalInput)
+    {
+        if (runInput == 0)
         {
             anim.SetBool("isWalking", false);
         }
@@ -40,7 +69,7 @@ public class PlayerController : MonoBehaviour
         {
             anim.SetBool("isWalking", true);
 
-            if (moveInput > 0)
+            if (runInput > 0)
             {
                 transform.eulerAngles = new Vector2(0, 0);
             }
